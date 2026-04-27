d56c8c075b8e7add451b6018dcd299575c6d1fe3
Ghostz_maker <sumitra.2913@gmail.com>
2026-04-27 21:15:41 +0530

    updated with new concept

--- practiceapp2/views.py
+++ practiceapp2/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here. #added 24-04-26
 def home(request):
     return render(request, 'practiceapp2/index.html') 
+
+#updated 27-04-26
+def about(request):
+    return render(request, 'practiceapp2/about.html')
+
+def contact(request):
+    return render(request, 'practiceapp2/contact.html')
